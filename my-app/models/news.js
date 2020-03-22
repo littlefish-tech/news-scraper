d9fad608432d9d5db70e51bdfdbf814f20a05513
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-21 22:04:04 -0700

    set up models

--- my-app/models/news.js
+++ my-app/models/news.js
@@ -1,1 +1,13 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const newsSchema = new Schema({
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    image: {type: String, required: true },
+    articalLink: { type: String, require: true }
+})
+
+const News = mongoose.model("News", newsSchema);
+
+module.exports = News;
