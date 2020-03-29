e4d0ed160d6fbd9302cb440423d376cc33e88d65
Yimeng Yu <yimeng.yu@gmail.com>
2020-03-28 22:36:59 -0700

    update new component

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -9,12 +9,13 @@ import { Delete, ContactsOutlined } from '@material-ui/icons';
 import { ListItemSecondaryAction,  IconButton} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import SaveBtn from"../components/SaveBtn";
+import New from "../components/News/New"
 
 const styles = {  root: { margin: 20, padding: 20, maxWidth: 400 },
 form: { display: 'flex', alignItems: 'baseline', justifyContent: 'space-evenly' }};
 
-// export default withStyles(styles)( class News extends Component {
-    class News extends Component {
+export default withStyles(styles)( class News extends Component {
+    // class News extends Component {
     state = {
         news: [],
         savedArticles: [],
@@ -59,27 +60,26 @@ form: { display: 'flex', alignItems: 'baseline', justifyContent: 'space-evenly' 
 
     render(){
         
-        // const { title, exercises } = this.state 
-        // const { classes } = this.props
+        // const { title, newsLink } = this.state 
+        const { classes } = this.props
         return(
-            
-            // <Paper className={classes.root}>
-            // <Typography variant='h3' align='center' gutterBottom>        Exercises      </Typography>
-            // <form><TextField name="title" label="Exercise" value={title} onChange={this.handleChange} margin='normal' />
-            // <Button type="submit" color="primary" variant="raised">Create</Button>
-            // </form>
-            // <List>{this.state.exercises.map(({ id, title }) => <ListItem key={id}>            <ListItemText primary={title} /> <ListItemSecondaryAction><IconButton color='primary' onClick={() => this.handleDelete(id)}>
-            //  <SaveBtn></SaveBtn>   
-            //       <Delete /> </IconButton> </ListItemSecondaryAction> </ListItem>)}
-           
-            // </List>
-            // </Paper>
-            <div>Hello</div>
+            <Paper className={classes.root}>
+            <News>
+                {this.state.news.map(newArt => {
+                    return (
+                        <New 
+                        title={this.state.title}
+
+                        />
+                    )
+                })}
+            </News>
+            </Paper>
         )}
 
 }
 
-// )
+)
 
-export default News;
+// export default News;
 
